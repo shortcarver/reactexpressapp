fc4eaf00f85a93feec0e2e55205243dd8bd37098
Michael Carver <michael.j.carver@gmail.com>
2018-09-09 00:03:33 -0500

    Added material ui

--- react/app.js
+++ react/app.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { render } from 'react-dom';
+import Button from '@material-ui/core/Button';
 
 if (module.hot) {
   module.hot.accept();
 }
 
 const MainApp = () => (
-  <h1>Hello React!!!!!</h1>
+  <Button variant="raised" color="primary">Hellow!</Button>
 );
 // render the app
 render(<MainApp />, document.getElementById('app'));
