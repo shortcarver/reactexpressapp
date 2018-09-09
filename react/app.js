9037431ebf338e621a3c76691f51368df2ef7f6f
Michael Carver <michael.j.carver@gmail.com>
2018-09-08 23:53:35 -0500

    react

---
+++ react/app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+if (module.hot) {
+  module.hot.accept();
+}
+
+const MainApp = () => (
+  <h1>Hello React!</h1>
+);
+// render the app
+render(<MainApp />, document.getElementById('app'));
