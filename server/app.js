08888f1b5b2e7ca554534c812f45c0d77c5a1385
Michael Carver <michael.j.carver@gmail.com>
2018-09-08 23:40:50 -0500

    Got error handling working

---
+++ server/app.js
@@ -0,0 +1,35 @@
+import path from 'path';
+import express from 'express';
+import routes from './routes/index';
+
+// Express app setup
+const app = express();
+
+// view engine
+app.set('views', path.join(__dirname, './views'));
+app.set('view engine', 'pug');
+
+// serve static files from 'public'
+app.use(express.static(path.join(__dirname, '../public')));
+
+// use routes
+app.use('/', routes);
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: app.get('env') === 'development' ? err : {},
+  });
+  next();
+});
+
+export default app;
